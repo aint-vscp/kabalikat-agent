5c8160931221ce5a1d31754e6eeb1fce27602dba
Vash Puno (VSCP) <vashpuno2004@gmail.com>
2026-01-23 21:04:58 +0800

    Fixed Google Auth

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -11,7 +11,8 @@ const config: CapacitorConfig = {
       "oauth2.googleapis.com",
       "www.googleapis.com"
     ]
-  }
+  },
+  overrideUserAgent: "Mozilla/5.0 (Linux; Android 14; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
 };
 
 export default config;
